89ea69e07a0373e17f666b9b6a18573ec9fddb43
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-04-22 21:22:15 +0530

    header mobile version

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,47 +1,64 @@
 import React from "react";
 import logo from "../assets/logo.webp";
 import Search from "./Search";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
 import useMobile from "../hooks/useMobile";
 const Header = () => {
-    const [isMobile]=useMobile();
-    console.log(isMobile)
+  const [isMobile] = useMobile();
+  const location = useLocation();
+  const isSearchpage = location.pathname === "/search";
+  console.log("is search page", isSearchpage);
+  console.log("ismobile", isMobile);
   return (
     <header className="h-26 md:h-18 md:shadow-md sticky top-0 flex md:items-center justify-around md:flex-row flex-col md:justify-between">
-      <div className="container mx-auto px-3 flex items-center justify-between">
-        {/* logo */}
-        <Link to={"/"} className="flex h-full items-center">
-          <div>
-            {/* pc */}
-            <img src={logo} alt="logo" width={150} height={60} className="hidden lg:block"/>
-
-            {/* mobile */}
-            <img src={logo} alt="logo" width={100} height={40} className="lg:hidden block"/>
-          </div>
-        </Link>
-
-        {/* search */}
-        <div className="hidden md:block">
-           
-                <Search/>
-            
-        </div>
-
-        {/* login */}
-
-        <div className="hidden md:block">
+      
+      {/* mobile verison */}
+      
+      {!(isSearchpage && isMobile) && (
+        <div className="container mx-auto px-3 flex items-center justify-between">
+          {/* logo */}
+          <Link to={"/"} className="flex h-full items-center">
             <div>
-               login
-            </div>
-        </div>
+              {/* pc */}
+              <img
+                src={logo}
+                alt="logo"
+                width={150}
+                height={60}
+                className="hidden lg:block"
+              />
 
-        <div className="md:hidden block">
-            <FaRegUserCircle size={26}/>
+              {/* mobile */}
+              <img
+                src={logo}
+                alt="logo"
+                width={100}
+                height={40}
+                className="lg:hidden block"
+              />
+            </div>
+          </Link>
+
+          {/* search */}
+          <div className="hidden md:block">
+            <Search />
+          </div>
+
+          {/* login */}
+
+          <div className="hidden md:block">
+            <div>login</div>
+          </div>
+
+          <div className="md:hidden block">
+            <FaRegUserCircle size={26} />
+          </div>
         </div>
-      </div>
+      )}
+
       <div className="md:hidden block container mx-auto px-3">
-        <Search/>
+        <Search />
       </div>
     </header>
   );
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,61 +1,75 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { useLocation, useNavigate } from 'react-router-dom';
-import { TypeAnimation } from 'react-type-animation';
+import { useLocation, useNavigate } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
+import { FaArrowLeft } from "react-icons/fa6";
+import useMobile from "../hooks/useMobile";
 const Search = () => {
-    const navigate=useNavigate()
-    const location=useLocation()
-    const [isSearchpage,setIsSearchPage]=useState()
-    useEffect(()=>{
-        const isSearch=location.pathname==="/search"
-        setIsSearchPage(isSearch)
-        console.log(isSearch)
-    },[location])
-    const redirectToSearchPage=()=>{
-        navigate("/search")
-    }
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [isMobile] = useMobile();
+  const [isSearchpage, setIsSearchPage] = useState();
+  useEffect(() => {
+    const isSearch = location.pathname === "/search";
+    setIsSearchPage(isSearch);
+    console.log(isSearch);
+  }, [location]);
+  const redirectToSearchPage = () => {
+    navigate("/search");
+  };
+  const redirectToHome = () => {
+    navigate("/");
+  };
   return (
     <div className="w-full border min-w-[300px] md:min-w-[400px] h-10 rounded flex items-center text-neutral-600 group focus-within:border-amber-300">
-      <button className="flex h-full items-center p-3 text-neutral-600 group-focus-within:text-amber-300">
-        <IoSearch size={20} />
-      </button>
-      <div className='w-full'>
-        <div className='w-full'>
-            {!isSearchpage ?
-             <div onClick={redirectToSearchPage} className='w-full'>
-
-             <TypeAnimation className='w-full'
-               sequence={[
-                 
-                 "Search 'Milk'",
-                 1000,
-                 "Search 'Sugar'",
-                 1000,
-                 "Search 'Chiken'",
-                 1000,
-                "Search 'Rice'",
-                 1000,
-                 "Search 'Chocolate'",
-                 1000,
-                 "Search 'Cake'",
-                 1000
-               ]}
-               wrapper="span"
-               speed={50}
-               repeat={Infinity}
-             />
-             </div>
-             :
-             <div className='w-full'>
-
-                <input type='text' placeholder='Search for ...' className='w-full outline-none' autoFocus={true}/>
-                </div>
-                }
+      {isSearchpage && isMobile ? (
+        <button className="flex h-full items-center p-3 text-neutral-600 group-focus-within:text-amber-300 cursor-pointer bg-white rounded-full m-1  shadow-lg" onClick={redirectToHome}>
+          <FaArrowLeft size={20} />
+        </button>
+      ) : (
+        <button className="flex h-full items-center p-3 text-neutral-600 group-focus-within:text-amber-300 ">
+          <IoSearch size={20} />
+        </button>
+      )}
+      <div className="w-full">
+        <div className="w-full">
+          {!isSearchpage ? (
+            <div onClick={redirectToSearchPage} className="w-full">
+              <TypeAnimation
+                className="w-full"
+                sequence={[
+                  "Search 'Milk'",
+                  1000,
+                  "Search 'Sugar'",
+                  1000,
+                  "Search 'Chiken'",
+                  1000,
+                  "Search 'Rice'",
+                  1000,
+                  "Search 'Chocolate'",
+                  1000,
+                  "Search 'Cake'",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </div>
+          ) : (
+            <div className="w-full">
+              <input
+                type="text"
+                placeholder="Search for ..."
+                className="w-full outline-none"
+                autoFocus={true}
+              />
+            </div>
+          )}
         </div>
-       
       </div>
     </div>
   );
-}
+};
 
-export default Search
+export default Search;
