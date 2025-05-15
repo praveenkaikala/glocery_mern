ce5d13572f1a78670c4fede8cc4ebfd76d705158
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-15 10:40:29 +0530

    user slice

---
+++ frontend/src/store/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+ const userSlice=createSlice({
+    name:"user",
+    initialState:{
+        name:null,
+        email:null,
+        avatar:null,
+        _id:null,
+    },
+    reducers:{
+        setUser:(state,action)=>{
+            state.name=action.payload.name
+            state.email=action.payload.email
+            state.avatar=action.payload.avatar
+            state._id=action.payload._id
+        },
+        removeUser:(state)=>{
+            state.name=null
+            state.email=null
+            state.avatar=null
+            state._id=null
+        }
+    }
+})
+
+export const {setUser,removeUser}=userSlice.actions
+export default userSlice.reducer
