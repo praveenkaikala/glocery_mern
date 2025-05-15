666da5914079fdada024f160a52a30e9a40e957d
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-15 10:28:13 +0530

    redux setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,13 @@ import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './route/router.jsx'
 import { Toaster } from 'react-hot-toast'
-
+import {Provider} from "react-redux"
+import { store } from './store/store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>
+    <Provider store={store}>
    <RouterProvider router={router}/>
    <Toaster />
-    </>
+    </Provider>
   </StrictMode>,
 )
