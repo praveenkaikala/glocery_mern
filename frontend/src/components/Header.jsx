b78e40da9fe0aec05e43062bea8e0410dc84991d
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-17 20:36:00 +0530

    adding user menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.webp";
 import Search from "./Search";
 import { Link, useLocation } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
+import { GoTriangleUp } from "react-icons/go";
+import { GoTriangleDown } from "react-icons/go";
 import useMobile from "../hooks/useMobile";
 import { TiShoppingCart } from "react-icons/ti";
+import { useSelector } from "react-redux";
+import UserMenu from "./UserMenu";
 const Header = () => {
   const [isMobile] = useMobile();
   const location = useLocation();
+  const [showuserMenu,setUserShowMenu]=useState(false)
+  const user=useSelector(state=>state?.user)
   const isSearchpage = location.pathname === "/search";
   console.log("is search page", isSearchpage);
   console.log("ismobile", isMobile);
+  const handleUserMenu=()=>{
+    setUserShowMenu(!showuserMenu)
+  }
   return (
     <header className="h-26 md:h-18 md:shadow-md sticky top-0 flex md:items-center justify-around md:flex-row flex-col md:justify-between bg-white">
       
@@ -49,7 +58,29 @@ const Header = () => {
           {/* login */}
 
           <div className="hidden md:flex items-center gap-4">
-            <Link to={"/login"}><button className="font-semibold text-md cursor-pointer">Login</button></Link>
+            {
+              user?._id ? (
+                <div className="relative w-auto" >
+                  <div className="flex gap-2 p-2 rounded items-center cursor-pointer hover:bg-green-800 hover:text-white transition-all ease-in-out duration-300" onClick={handleUserMenu}>
+                    <p>Account</p>
+                    {
+                      showuserMenu ?(
+                        <GoTriangleUp/>
+                      ):(
+
+                        <GoTriangleDown />
+                      )
+                    }
+                    </div>
+                    <div className="absolute right-10 top-14">
+                     { showuserMenu && <UserMenu/>}
+                      </div>
+                  </div>
+              ):(
+
+                <Link to={"/login"}><button className="font-semibold text-md cursor-pointer">Login</button></Link>
+              )
+            }
             <button className="flex items-center gap-2 bg-green-900 text-white px-4 py-2 rounded cursor-pointer">
               <div className="animate-bounce">
               <TiShoppingCart size={26}/>
