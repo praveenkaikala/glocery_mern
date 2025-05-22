a3b56a336b07a9a345cb6138801a339e59db2300
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-22 20:19:21 +0530

    update user logc

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { FaUserCircle } from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import UserProfileEdit from '../components/UserProfileEdit';
+import { toastError } from '../utils/toastError';
+import { AxiosPravite } from '../utils/Axios';
+import { summaryApi } from '../common/SummaryApi';
+import { updateUser } from '../store/userSlice';
+import { toastSuccess } from '../utils/toastSuccess';
 
 const Profile = () => {
   const user=useSelector(state=>state?.user);
+  const dispatch=useDispatch()
+  const [loading,setLoading]=useState(false)
   const [userdetails,setUserDetails]=useState({
     name:user?.name,
     email:user?.email,
@@ -27,6 +34,23 @@ const Profile = () => {
       ...prev,
       [e.target.name]:e.target.value}
     })
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    try {
+      setLoading(true)
+      const resp=AxiosPravite({
+        ...summaryApi.updateDetails,
+        data:userdetails
+      })
+      dispatch(updateUser(userdetails));
+      toastSuccess(resp?.data?.message || "Detaills updated Successfully")
+    } catch (error) {
+    toastError("Failed To Update")
+    }
+    finally{
+      setLoading(false)
+    }
   }
   return (
     <section>
@@ -52,17 +76,17 @@ const Profile = () => {
     <form className="grid gap-3">
           <div className='grid gap-1'>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" className='bg-blue-50 outline-none border rounded focus-within:border-amber-300 py-1 px-2' value={userdetails?.name}/>
+            <input type="text" id="name" name="name" className='bg-blue-50 outline-none border rounded focus-within:border-amber-300 py-1 px-2' value={userdetails?.name}  onChange={handleChange}/>
           </div>
           <div className='grid gap-1'>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" className=' bg-blue-50 outline-none border rounded focus-within:border-amber-300 py-1 px-2' value={userdetails?.email}/>
+            <input type="email" id="email" name="email" className=' bg-blue-50 outline-none border rounded focus-within:border-amber-300 py-1 px-2' value={userdetails?.email}  onChange={handleChange}/>
           </div>
           <div className='grid gap-1'>
             <label htmlFor="name">Mobile</label>
-            <input type="number" id="mobile" name="mobile" className=' bg-blue-50 outline-none border rounded focus-within:border-amber-300 py-1 px-2' value={userdetails?.mobile}/>
+            <input type="number" id="mobile" name="mobile" className=' bg-blue-50 outline-none border rounded focus-within:border-amber-300 py-1 px-2' value={userdetails?.mobile}  onChange={handleChange}/>
           </div>
-          <button className='border border-amber-300 py-2 rounded-md hover:bg-amber-300 transition-all ease-in-out cursor-pointer'>Save Datails</button>
+          <button className='border border-amber-300 py-2 rounded-md hover:bg-amber-300 transition-all ease-in-out cursor-pointer' onClick={handleSubmit}>{loading?"saving....":"Save Datails"}</button>
           </form>
     </section>
   )
