088c686acf543364987d7233f85938643596468e
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-24 20:22:14 +0530

    category page

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UploadCategory from '../components/UploadCategory'
 
 const Category = () => {
+    const [showUploadCategory,setShowUploadCategory]=useState(false)
   return (
-    <div>Category</div>
+    <section>
+        <div className='p-2 container bg-white shadow flex items-center gap-4'>
+            <h2 className='font-semibold'>Category</h2>
+            <button className='border border-amber-300 hover:bg-amber-300 transition-all ease-in-out rounded py-1 px-2 cursor-pointer' onClick={()=>setShowUploadCategory(true)}>Add Category</button>
+        </div>
+        {
+            showUploadCategory && <UploadCategory close={()=>setShowUploadCategory(false)}/>
+        }
+    </section>
   )
 }
 
