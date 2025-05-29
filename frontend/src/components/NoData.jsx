1e32d3187fd4bde25a3cee1908a7c0e8772f6cec
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-29 20:30:06 +0530

    no data page

--- frontend/src/components/NoData.jsx
+++ frontend/src/components/NoData.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import noData from "../assets/nodata.webp"
 const NoData = () => {
   return (
-    <div>NoData</div>
+    <div className='flex justify-center flex-col items-center'>
+        <img
+        src={noData}
+        loading='lazy'
+        className='w-50'
+        />
+        <h2 className='font-semibold text-xl'>No Data</h2>
+    </div>
   )
 }
 
