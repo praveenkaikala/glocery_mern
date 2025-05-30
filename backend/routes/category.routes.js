a61ce6a62b8f680ebd901575d47c017d7dcf101b
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-30 19:47:03 +0530

    category routes backend

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { upload } from "../middleware/multer.js"
-import { createCategoryController, getCategoryController } from "../controllers/category.controller.js"
+import { createCategoryController, getCategoryController, UpdateCategoryController } from "../controllers/category.controller.js"
 import { authMiddleware } from "../middleware/auth.js";
 import { adminAuth } from "../middleware/adminAuth.js";
 const router=express.Router()
 
 router.post("/create-category",authMiddleware,adminAuth,upload.single("image"),createCategoryController);
+router.put("/update-category",authMiddleware,adminAuth,upload.single("image"),UpdateCategoryController);
 router.get("/get-category",authMiddleware,adminAuth,getCategoryController)
 export default router
