74920499fe30da7b2ed2f84ba4b067cb65d258b7
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-31 20:37:02 +0530

    edit and delete buttons in category

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -26,7 +26,7 @@ const Category = () => {
     fetchCategoryList();
   }, []);
   return (
-    <section >
+    <section>
       <div className="p-2 container bg-white shadow flex items-center gap-4">
         <h2 className="font-semibold">Category</h2>
         <button
@@ -46,24 +46,34 @@ const Category = () => {
       )}
       {!loading && !categoryData && (
         <div>
-         <NoData/>
+          <NoData />
         </div>
       )}
-      {
-        categoryData.length>0 && 
+      {categoryData.length > 0 && (
         <div className=" grid grid-cols-2 md:grid-cols-5 gap-4 p-3 ">
-          {
-            categoryData?.map((data,ind)=>{
-              return(
-                <div className="h-40 p-3 border-b-2 border-red-500 rounded-xl group cursor-pointer hover:bg-gray-100 flex items-center flex-col z-1">
-                  <img src={data?.image} alt={data?.name} className="w-32 h-28 object-scale-down group-hover:scale-[1.1] rounded-xl transition-all ease-in-out duration-500"  />
-                  <h2>{data?.name}</h2>
-                  </div>
-              )
-            })
-          }
+          {categoryData?.map((data, ind) => {
+            return (
+              <div className="h-50 p-3 border-b-2 border-red-500 rounded-xl group cursor-pointer hover:bg-gray-100 flex items-center flex-col z-1">
+                <img
+                  src={data?.image}
+                  alt={data?.name}
+                  className="w-32 h-28 object-scale-down group-hover:scale-[1.1] rounded-xl transition-all ease-in-out duration-500"
+                />
+                <h2>{data?.name}</h2>
+
+                <div className="group-hover:opacity-100 group-hover:scale-100 opacity-0 scale-95 transform transition-all duration-300 ease-in-out gap-2 py-2 flex">
+                  <button className="px-3 text-green-500 border border-green-500 rounded cursor-pointer hover:bg-green-500 hover:text-white transition-all duration-300 ease-in-out">
+                    Edit
+                  </button>
+                  <button className="px-3 text-red-500 border border-red-500 rounded cursor-pointer hover:bg-red-500 hover:text-white transition-all duration-300 ease-in-out">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
-      }
+      )}
     </section>
   );
 };
