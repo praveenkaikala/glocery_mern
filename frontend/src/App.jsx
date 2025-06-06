ff4c7af6d26409af7e603f5f5c28379b0029488b
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-06-06 19:41:38 +0530

    setting category data data to reduv

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ import { useEffect } from "react";
 import { fetchUserDatail } from "./utils/fetchUserdetails";
 import { useDispatch } from "react-redux";
 import { setUser } from "./store/userSlice";
+import { setCategory } from "./store/productSlice";
+import { summaryApi } from "./common/SummaryApi";
+import { AxiosPravite } from "./utils/Axios";
 function App() {
   const dispatch=useDispatch()
   const fetchUser = async () => {
@@ -18,8 +21,25 @@ function App() {
       console.log("error",error)
     }
    }
+   const fetchCategoryList = async () => {
+       try {
+         
+         const resp = await AxiosPravite({
+           ...summaryApi.getCategotyList,
+         });
+         console.log("cat",resp.data.data.length)
+         if(resp.data.data.length>0)
+         {
+          console.log("calling category reducer")
+          dispatch(setCategory(resp.data.data))
+         }
+       } catch (error) {
+         console.log(error);
+       } 
+     };
   useEffect(()=>{
     fetchUser()
+    fetchCategoryList()
   },[])
   return(
     <>
