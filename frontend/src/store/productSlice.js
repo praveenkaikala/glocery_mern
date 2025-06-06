aefc1e32d19258b1edaab70f26b6f747abfd2617
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-06-06 19:33:36 +0530

    reducers

--- frontend/src/store/productSlice.js
+++ frontend/src/store/productSlice.js
@@ -9,6 +9,25 @@ const initialValue={
 const productSlice=createSlice({
     name:"product",
     initialState:initialValue,
-    
+    reducers:{
+        setCategory:(state,action)=>{
+           
+                state.category=[...action.payload]
+           
+        },
+        setSubCategory:(state,action)=>{
+           
+                state.subCategory=[...action.payload]
+           
+        },
+        setProduct:(state,action)=>{
+           
+                state.product=[...action.payload]
+           
+        }
+    }
 
 })
+
+export const {setCategory,setProduct,setSubCategory}=productSlice.actions
+export default productSlice.reducer
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
+import productReducer from "./productSlice"
 
 export const store = configureStore({
   reducer: {
     user:userReducer,
+    product:productReducer
   },
   devTools:true,
 })
