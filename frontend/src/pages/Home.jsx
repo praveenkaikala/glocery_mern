e89ff5b5d270d980e395207251b652c07ec54e7b
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-07-02 19:25:21 +0530

    adding banners

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import banner from "../assets/banner.jpg"
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='bg-white'>
+      <div className='container mx-auto'>
+          <div className={`bg-green-100 w-full min-h-44 rounded ${!banner && "animate-pulse"}`}>
+                 <img
+                 src={banner}
+                 className='w-full h-full'
+                 alt='banner'
+                 loading='lazy'
+                 />
+          </div>
+      </div>
+    </section>
   )
 }
 
