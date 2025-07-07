a6cf33d42bf562569afa5ce45ffb8b464b9eff6d
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-07-07 17:49:44 +0530

    cateory

--- frontend/src/components/CategoryWiseProducts.jsx
+++ frontend/src/components/CategoryWiseProducts.jsx
@@ -73,7 +73,7 @@ const CategoryWiseProductDisplay = ({ id, name }) => {
                 <h3 className='font-semibold text-lg md:text-xl'>{name}</h3>
                 <Link  to={redirectURL} className='text-green-600 hover:text-green-400'>See All</Link>
             </div>
-             {
+             { loading &&
                         <CardSkeleton number={ismobile ? 2 :6}/>
                     }
            {
