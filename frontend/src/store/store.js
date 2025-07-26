11cc1279d8de79023c5524a4b5f9839362686d46
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-07-26 09:35:16 +0530

    cart reducer

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 import productReducer from "./productSlice"
-
+import cartReducer from "./cartSlice"
 export const store = configureStore({
   reducer: {
     user:userReducer,
-    product:productReducer
+    product:productReducer,
+    cart:cartReducer
   },
   devTools:true,
 })
