ef2373991806c925e25dd02857338846d5a7a1f0
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-08-09 11:11:08 +0530

    schemma change

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -1,52 +1,66 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const orderSchema=mongoose.Schema({
-    userId:{
-        type:mongoose.Schema.ObjectId,
-        ref:"user"
+const orderSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "user",
     },
-    orderId:
-    {
-        type:String,
-        required:[true,"provide orderid"],
-        unique:true
+    orderId: {
+      type: String,
+      required: [true, "provide orderid"],
+      unique: true,
     },
-    product_id:{
-        type:mongoose.Schema.ObjectId,
-        ref:"product"
+    products: [
+      {
+        product_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "product",
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        image: {
+          type: String,
+          default: "",
+        },
+        qunatity: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+    payment_id: {
+      type: String,
+      default: "",
     },
-    product_details:{
-        name:String,
-        image:Array,
+    payment_status: {
+      type: String,
+      default: "",
+    },
+    delivery_address: {
+      type: mongoose.Schema.ObjectId,
+      ref: "address",
+    },
+    sub_total_amount: {
+      type: Number,
+      default: 0,
+    },
+    total_amount: {
+      type: Number,
+      default: 0,
+    },
+    invoice_recipt: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-    },
-    payment_id:{
-        type:String,
-        default:"",
-    },
-    payment_status:{
-        type:String,
-        default:""
-    },
-    delivery_address:{
-        type:mongoose.Schema.ObjectId,
-        ref:"address"
-    },
-    sub_total_amount:{
-        type:Number,
-        default:0
-    },
-    total_amount:{
-        type:Number,
-        default:0 
-    },
-    invoice_recipt:{
-         type:String,
-        default:""  
-    }
-},{
-    timestamps:true
-})
-
-const orderModel=mongoose.model("order",orderSchema)
-export default orderModel
+const orderModel = mongoose.model("order", orderSchema);
+export default orderModel;
